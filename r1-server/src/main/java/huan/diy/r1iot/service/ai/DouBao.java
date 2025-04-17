78f571327454e9810c260a5b15c6756828e2a5d1
chenjx7 <49031812+chenjx7@users.noreply.github.com>
2025-04-17 23:47:28 +0800

    Update DouBao.java

--- r1-server/src/main/java/huan/diy/r1iot/service/ai/DouBao.java
+++ r1-server/src/main/java/huan/diy/r1iot/service/ai/DouBao.java
@@ -1,1 +1,18 @@
+package huan.diy.r1iot.service.ai;
 
+import org.springframework.stereotype.Service;
+
+
+@Service("DouBao")
+public class DouBao extends GrokAiX {
+
+    public DouBao() {
+        this.BASE_URL = "https://ark.cn-beijing.volces.com/api/v3";
+        this.MODEL = "doubao-pro-32k-functioncall-241028";
+    }
+
+    @Override
+    public String getAlias() {
+        return "DouBao";
+    }
+}
