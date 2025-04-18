f95f29f1689f473c2e29e36e5e465620cf72c299
chenjx7 <49031812+chenjx7@users.noreply.github.com>
2025-04-19 04:22:51 +0800

    Update Groq.java

--- r1-server/src/main/java/huan/diy/r1iot/service/ai/Groq.java
+++ r1-server/src/main/java/huan/diy/r1iot/service/ai/Groq.java
@@ -1,1 +1,18 @@
+package huan.diy.r1iot.service.ai;
 
+import org.springframework.stereotype.Service;
+
+
+@Service("Groq")
+public class Groq extends GrokAiX {
+
+    public Groq() {
+        this.BASE_URL = "https://bitter-chicken-18.deno.dev/api.groq.com/openai";
+        this.MODEL = "deepseek-r1-distill-llama-70b";
+    }
+
+    @Override
+    public String getAlias() {
+        return "Groq";
+    }
+}
