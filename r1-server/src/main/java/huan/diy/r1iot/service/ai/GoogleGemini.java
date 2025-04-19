3c7a65ecb8f556963005cbdc50dae6c913a763e6
chenjx7 <49031812+chenjx7@users.noreply.github.com>
2025-04-20 00:07:09 +0800

    Update GoogleGemini.java

--- r1-server/src/main/java/huan/diy/r1iot/service/ai/GoogleGemini.java
+++ r1-server/src/main/java/huan/diy/r1iot/service/ai/GoogleGemini.java
@@ -5,7 +5,6 @@ import dev.langchain4j.model.chat.ChatLanguageModel;
 import huan.diy.r1iot.model.Device;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,13 +17,17 @@ public class GoogleGemini implements IAIService {
     private RestTemplate restTemplate;
 
     @Autowired
-    private ObjectMapper objectMapper;
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final String API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key="; // 替换为实际的 API URL
 
     @Override
     public ChatLanguageModel buildModel(Device device) {
-        log.info("Building Gemini model with device: {}", device.getName());
+        // todo add langchain gemini dependency
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-    
+
+
     @Override
     public String getAlias() {
         return "Gemini";
