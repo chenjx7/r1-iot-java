e4afb07b40052b19ccd317b705ca0201cf8cdfd5
chenjx7 <49031812+chenjx7@users.noreply.github.com>
2025-04-19 23:32:14 +0800

    Update GoogleGemini.java

--- r1-server/src/main/java/huan/diy/r1iot/service/ai/GoogleGemini.java
+++ r1-server/src/main/java/huan/diy/r1iot/service/ai/GoogleGemini.java
@@ -2,7 +2,6 @@ package huan.diy.r1iot.service.ai;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.gemini.GeminiChatModel; // 修正导入
 import huan.diy.r1iot.model.Device;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,10 +23,6 @@ public class GoogleGemini implements IAIService {
     @Override
     public ChatLanguageModel buildModel(Device device) {
         log.info("Building Gemini model with device: {}", device.getName());
-        return GeminiChatModel.builder()
-                .apiKey(device.getAiConfig().getKey())
-                .modelName("gemini-2.0-flash") // 可以根据需要选择不同的模型
-                .build();
     }
     
     @Override
